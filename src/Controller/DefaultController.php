970f9cd5a39b2c2d69dd2a4bb1215a507cfb0c35
moussa <moussabaye94@gmail.com>
2020-10-13 13:24:49 +0200

    adding controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    
+    /** 
+     * @Route("/", name="accueil")
+    */
+    public function accueil()
+    {
+        return $this->render('default/accueil.html.twig');
+    }
+    
+
+    /**
+     * @Route("/index", name="index")
+     * 
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'day' => "mardi",
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    
+}
